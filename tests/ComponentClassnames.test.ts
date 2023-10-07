ced5e29efe7e1a4b2a1cd7606a6b4930c250de9c
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2023-10-07 16:48:08 -0400

    Add constructor tests

---
+++ tests/ComponentClassnames.test.ts
@@ -0,0 +1,80 @@
+import ccn from '../src/ComponentClassnames.ts'
+import type { CustomCSS } from '../src/ComponentClassnames.ts'
+import { it, describe, test, expect } from 'bun:test'
+
+const stylesheet1 = {
+  Component: 'hashed-component-name',
+  Component__child: 'hashed-child_component-name'
+}
+
+const stylesheet2 = {
+  Block: 'hashed-block-name',
+  Block__child: 'hashed-child_element-name'
+}
+
+describe('CustomCSS constructor', () => {
+
+  test('Creates expected object with no arguments', () => {
+    const customCSS: CustomCSS = ccn.CustomCSS()
+
+    expect(customCSS).toEqual({
+      unstyled: false,
+      stylesheets: [],
+      classNames: {},
+      styles: {},
+      modifiers: {}
+    })
+  })
+
+  test("correctly assigns object passed-in values", () => {
+    const customCSS1 = ccn.CustomCSS({ stylesheets: [stylesheet1, stylesheet2] })
+    const customCSS2 = ccn.CustomCSS({
+      classNames: {
+        Thing: ['class1', { class2: true }]
+      },
+      modifiers: {
+        Thing: ['hello']
+      }
+    })
+    const customCSS3 = ccn.CustomCSS({
+      styles: {
+        Block: {
+          backgroundColor: 'green'
+        }
+      },
+      unstyled: true
+    })
+
+    expect(customCSS1).toEqual({
+      unstyled: false,
+      stylesheets: [stylesheet1, stylesheet2],
+      classNames: {},
+      styles: {},
+      modifiers: {}
+    })
+
+    expect(customCSS2).toEqual({
+      unstyled: false,
+      stylesheets: [],
+      classNames: {
+        Thing: ['class1', { class2: true }]
+      },
+      styles: {},
+      modifiers: {
+        Thing: ['hello']
+      }
+    })
+
+    expect(customCSS3).toEqual({
+      unstyled: true,
+      stylesheets: [],
+      classNames: {},
+      styles: {
+        Block: {
+          backgroundColor: 'green'
+        }
+      },
+      modifiers: {}
+    })
+  })
+})
